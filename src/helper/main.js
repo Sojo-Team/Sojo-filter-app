a04db33bb70166dba508490b34951e3d318cea49
prabal <prabalrai17@gmail.com>
2023-12-10 15:30:16 +0545

    changes

--- src/helper/main.js
+++ src/helper/main.js
@@ -6,39 +6,79 @@ export function findCommonStrings(inputA, inputB) {
   // Get values from input fields
 
   // Split strings into arrays
-  var arrayA = inputA.split("\n").map((str) => removeSpaces(str).toLowerCase());
-  var arrayB = inputB.split("\n").map((str) => removeSpaces(str).toLowerCase());
+  var arrayA = inputA.split("\n");
+  var arrayB = inputB.split("\n");
+
+  // Remove spaces and split strings into arrays
+  // Remove spaces only when comparing
+  var arrayAWithoutSpaces = arrayA.map((str) =>
+    removeSpaces(str).toLowerCase()
+  );
+  var arrayBWithoutSpaces = arrayB.map((str) =>
+    removeSpaces(str).toLowerCase()
+  );
 
   // Find common strings
-  var commonStrings = arrayA.filter((value) => arrayB.includes(value));
+  var commonStringsWithoutSpaces = arrayAWithoutSpaces.filter((value) =>
+    arrayBWithoutSpaces.includes(value)
+  );
 
   // Display result
-  return commonStrings?.join("\n");
+
+  // Display result
+  return commonStringsWithoutSpaces
+    .map((value) => {
+      var originalIndex = arrayAWithoutSpaces.indexOf(value);
+      return arrayA[originalIndex];
+    })
+    .join("\n");
 }
 
 export function findOnlyInA(inputA, inputB) {
   // Get values from input fields
   // Remove spaces and split strings into arrays
-  var arrayA = inputA.split("\n").map((str) => removeSpaces(str).toLowerCase());
-  var arrayB = inputB.split("\n").map((str) => removeSpaces(str).toLowerCase());
 
-  // Find unique strings in A
-  var uniqueStringInA = arrayA.filter(
-    (value) => !arrayB.includes(removeSpaces(value))
+  // Split strings into arrays
+  var arrayA = inputA.split("\n");
+  var arrayB = inputB.split("\n");
+
+  // Remove spaces only when comparing
+  var arrayAWithoutSpaces = arrayA.map((str) =>
+    removeSpaces(str).toLowerCase()
+  );
+  var arrayBWithoutSpaces = arrayB.map((str) =>
+    removeSpaces(str).toLowerCase()
   );
 
-  return uniqueStringInA?.join("\n");
+  // Find unique in A
+  var onlyInA = arrayAWithoutSpaces.filter(
+    (value) => !arrayBWithoutSpaces.includes(value)
+  );
+
+  // Display result with original formatting
+  return onlyInA
+    ?.map((value) => arrayA[arrayAWithoutSpaces.indexOf(value)])
+    .join("\n");
 }
 export function findOnlyInB(inputA, inputB) {
   // Get values from input fields
   // Remove spaces and split strings into arrays
-  var arrayA = inputA.split("\n").map((str) => removeSpaces(str).toLowerCase());
-  var arrayB = inputB.split("\n").map((str) => removeSpaces(str).toLowerCase());
+  var arrayA = inputA.split("\n");
+  var arrayB = inputB.split("\n");
 
-  // Find unique strings in A
-  var uniqueStringInB = arrayB.filter(
-    (value) => !arrayA.includes(removeSpaces(value))
+  // Remove spaces only when comparing
+  var arrayAWithoutSpaces = arrayA.map((str) =>
+    removeSpaces(str).toLowerCase()
+  );
+  var arrayBWithoutSpaces = arrayB.map((str) =>
+    removeSpaces(str).toLowerCase()
+  );
+  var onlyInB = arrayBWithoutSpaces.filter(
+    (value) => !arrayAWithoutSpaces.includes(value)
   );
 
-  return uniqueStringInB?.join("\n");
+  // Display result with original formatting
+  return onlyInB
+    ?.map((value) => arrayB[arrayBWithoutSpaces.indexOf(value)])
+    .join("\n");
 }
